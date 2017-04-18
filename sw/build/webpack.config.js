cebc2f2d6463cf2472b678385b12e893d86378a2
liyh <lyh106415@alibaba-inc.com>
2017-04-18 23:56:44 +0800

    feat: webpack builder

--- sw/build/webpack.config.js
+++ sw/build/webpack.config.js
@@ -1,21 +1,38 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: {
-    index: './examples/app.js'
+    index: './app/app.js'
   },
   output: {
+    path: path.resolve('dist'),
     filename: 'bundle.js',
     library: 'bundle',
     libraryTarget: 'umd',
-    publicPath: '/examples/',
+    publicPath: '/',
     umdNamedDefine: true
   },
   module: {
-    loaders: [{
-      test: /\.js?$/,
+    rules: [{
+      test: /\.js$/,
       use: {
         loader: 'babel-loader'
       },
       exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
+      })
     }]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.[contenthash].css'),
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
 }
